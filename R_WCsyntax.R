0eae6925b8fdfa3cd4233c639f8034188ce2acba
Michael Siebel <michaeltsiebel@gmail.com>
2018-07-08 12:10:46 -0400

    Add files via upload

---
+++ R_WCsyntax.R
@@ -0,0 +1,147 @@
+# Coding Presentation (Stata v R v Python)
+# R Syntax
+# Fors Marsh Group
+# July 8, 2018
+# Author: Michael Siebel
+
+#######################
+##Load data set
+#set working directory
+setwd('C:\\Users\\Siebelm\\Documents\\4 FMG\\Coding Presentation')
+
+##Load data set
+#Import CSV & drop variables
+df = read.csv('results.csv') 
+df$city = NULL
+df$country = NULL
+
+#Check first few rows
+head(df)
+
+#Check variables
+sapply(df, class)
+
+#Check missings (NA's)
+sapply(df, function(x)sum(is.na(x)))
+
+#Subset dataset to only include Germany matches 
+df_ger = df[df$home_team=='Germany' | df$away_team=='Germany',]
+
+head(df_ger)
+
+
+##############################
+##Recodes and Data Manpulation
+#
+df_ger$match = 1
+
+tail(df_ger)
+
+#Conditional recode (Define Goals Scored)
+df_ger$goals    = ifelse(df_ger$home_team=='Germany', df_ger$home_score, ifelse(df_ger$away_team=='Germany', df_ger$away_score, NA))
+
+#Conditional recode (Define Goals Conceded)
+df_ger$conceded = ifelse(df_ger$home_team=='Germany', df_ger$away_score, ifelse(df_ger$away_team=='Germany', df_ger$home_score, NA))
+
+tail(df_ger)
+
+#Simple recode (Define Goal Differential)
+df_ger$goaldiff = df_ger$goals-df_ger$conceded
+
+tail(df_ger)
+
+#Dummy recode (Define Friendly v Competitive match)
+df_ger$friendly = as.logical(df_ger$tournament=='Friendly')
+
+tail(df_ger)
+
+#Categorical recode (home vs away vs neutral matches)
+df_ger$home[df_ger$home_team=='Germany'] = 'home'
+df_ger$home[df_ger$away_team=='Germany'] = 'away'
+df_ger$home[df_ger$neutral=='TRUE']      = 'neutral'
+
+tail(df_ger)
+
+#Conditional recode (opponent)
+df_ger$opponent = ifelse(df_ger$home_team=='Germany', as.character(df_ger$away_team), 
+                         ifelse(df_ger$away_team=='Germany', as.character(df_ger$home_team), NA))
+
+tail(df_ger)
+
+#String variables (Define year)
+df_ger$year = as.numeric(substr(df_ger$date,1,4))
+
+tail(df_ger)
+
+#Drop redundant variables
+df_ger = subset(df_ger, select = -c(home_score, away_score, tournament,
+               neutral, home_team, away_team))
+
+tail(df_ger)
+
+
+########################
+##Descriptive Statistics
+#Tabs
+tabHome = table(df_ger$home, df_ger$friendly)
+tabHome
+
+#Unweighted proportions
+propHome = prop.table(tabHome, 2)*100
+round(propHome, 2)
+
+#Goal difference table
+library('plyr')
+library('dplyr')
+goaldiffTable = group_by(df_ger, home, friendly)
+goaldiffTable = summarize(goaldiffTable,
+                       conceded = mean(conceded),
+                       goaldiff = mean(goaldiff),
+                       goals = mean(goals))
+goaldiffTable
+
+#Opponent table
+threeOpponents = df_ger[df_ger$opponent=='Mexico' | df_ger$opponent=='Sweden' | df_ger$opponent=='Korea Republic',]
+opponentTable = group_by(threeOpponents, opponent)
+opponentTable = summarize(opponentTable, 
+                          goaldiff = mean(goaldiff),
+                          match = sum(match))
+opponentTable
+
+
+#######################
+##Graphs
+#Histogram
+library('ggplot2')
+ggplot(df_ger, aes(goaldiff)) +
+  geom_histogram(fill = "blue", binwidth=.75, position="dodge") + 
+  ylab("Count") + xlab("Goal Differential") + ggtitle('Histogram of Goal Differences')
+
+ggplot(df_ger, aes(goaldiff, fill = friendly)) +
+  geom_histogram(binwidth=.75, position="dodge") + 
+  ylab("Count") + xlab("Goal Differential") + ggtitle('Histogram of Goal Differences') + 
+  theme(legend.position="bottom")
+
+#Line Graphs
+df_ger_byyear = ddply(df_ger, .(year), summarize,  goals=mean(goals), 
+                      conceded=mean(conceded), goaldiff=mean(goaldiff))
+df_ger_byyear$conceded = df_ger_byyear$conceded*-1
+tail(df_ger_byyear)
+
+df_goals_byyear = ggplot(df_ger_byyear) + geom_line(aes(x=year, y=goals), color='dark green') +
+  ylab("Goals") + xlab("Year") + ggtitle('Goals Scored by Year')
+df_goals_byyear
+
+df_conceded_byyear = ggplot(df_ger_byyear) + geom_line(aes(x=year, y=conceded), color='maroon') +
+  ylab("Conceded") + xlab("Year") + ggtitle('Goals Conceded by Year')
+df_conceded_byyear
+
+df_goaldiff_overlay_byyear = ggplot(df_ger_byyear) + geom_line(aes(x=year, y=goaldiff), color='blue') +
+  ylab("Goal Differential") + xlab("Year") + ggtitle('Goal Differential by Year') + labs(subtitle='Overlay Chart') +
+  geom_line(aes(x=year, y=goals), color='dark green') + geom_line(aes(x=year, y=conceded), color='maroon') +
+  geom_hline(yintercept = 0, color='dark grey')
+df_goaldiff_overlay_byyear
+
+
+
+
---
+++ Stata_WCsyntax.do
@@ -0,0 +1,155 @@
+set more off
+
+set more off, permanently
+set line 200
+capture log close
+log using Stata_WClog.smcl, replace
+
+************************
+**Load data set
+*set working directory
+cd "C:\Users\Siebelm\Documents\4 FMG\Coding Presentation"
+
+*Import CSV & drop variables
+import delimited using results.csv
+save results.dta, replace
+drop city country
+
+*Check first few rows
+list * in 1/10
+
+*Check variables
+describe
+
+*Check missings (dots or ".")
+findit mdesc
+mdesc
+
+*Subset dataset to only include Germany matches
+keep if home_team=="Germany" | away_team=="Germany"
+
+list * in 1/10
+
+
+************************
+**Recodes and Data Manpulation
+*Simple recode (each observation equals 1)
+gen match = 1
+
+list * in 1/10
+
+*Conditional recode (Define Goals Scored)
+gen goals = home_score if home_team=="Germany"
+replace goals = away_score if away_team=="Germany"
+
+*Conditional recode (Define Goals Conceded)
+gen conceded = away_score if home_team=="Germany"
+replace conceded = home_score if away_team=="Germany"
+
+list home_team away_team home_score away_score goals conceded in 921/931
+
+*Simple recode (Define Goal Differential)
+gen goaldiff = goals - conceded
+
+list home_score away_score goals conceded goaldiff in 921/931
+
+*Dummy recode (Define Friendly v Competitive match)
+gen friendly = tournament == "Friendly"
+replace friendly = . if tournament == "."
+label define frdlylabs 0 "FALSE" 1 "TRUE"
+label values friendly frdlylabs
+
+list tournament friendly in 921/931
+
+*Categorical recode (home vs away vs neutral matches)
+gen home = "home" if home_team=="Germany"
+replace home = "away" if away_team=="Germany"
+replace home = "neutral" if neutral=="TRUE"
+encode home, gen(home_num)
+label list home_num
+
+list home_team away_team neutral home home_num in 921/931
+
+*Conditional recode (opponent)
+gen opponent = away_team if home_team=="Germany"
+replace opponent = home_team if away_team=="Germany"
+
+list home_team away_team opponent in 921/931
+
+*String variables (Define year)
+gen year = substr(date,1,4)
+destring year, replace
+
+list date year in 921/931
+
+*Drop redundant variables
+drop home_score away_score tournament neutral home_team away_team
+
+list * in 921/931
+
+
+************************
+**Descriptive Statistics
+*Tabs
+tab home friendly
+
+*Unweighted proportions
+prop home_num friendly
+
+*Goal difference table
+table home friendly, contents(mean conceded)
+table home friendly, contents(mean goaldiff)
+table home friendly, contents(mean goals)
+	*or*
+forval frdly=0/1 {
+	foreach hme in away home neutral {
+		foreach var in conceded goaldiff goals {
+			local slabel: value label friendly
+			local vlabel: label `slabel' `frdly'
+			di as res _n "`var' if home is `hme' and friendly is `vlabel'" 
+			mean `var' if home=="`hme'" & friendly==`frdly'
+		}
+	}
+}
+
+*Opponent table
+mean goaldiff if opponent=="Korea Republic"
+tab match if opponent=="Korea Republic"
+
+
+************************
+**Graphs
+*Histogram
+hist goaldiff, bin(25) freq ///
+	title(Histogram of Goal Differences) ///
+	ytitle(Count) xtitle(Goal Differential) ///
+	title(Histogram of Goal Differential)
+
+*Line graphs
+bysort year: egen m_goals = mean(goals)
+twoway (line m_goals year, lcolor(green)), xlabel(1908(20)2018) ///
+	title(Goals Scored by Year)
+
+bysort year: egen m_conceded = mean(conceded)
+replace m_conceded = m_conceded*-1
+twoway (line m_conceded year, lcolor(red)), xlabel(1908(20)2018) ///
+	title(Goals Conceded by Year)
+
+bysort year: egen m_goaldiff = mean(goaldiff)
+twoway (line m_goaldiff year, lcolor(navy)), xlabel(1908(20)2018) ///
+	title(Goal Differential by Year)
+
+twoway (line m_goaldiff year, lcolor(navy)) ///
+	(line m_goals year, lcolor(green) lwidth(thin)) ///
+	(line m_conceded year, lcolor(red) lwidth(thin)), ///
+	xlabel(1908(30)2018) yline(0, lcolor(grey) lwidth(thin)) ///
+	title(Goal Differential by Year) subtitle(Overlay Chart) ///
+	ytitle(Year)
+
+translate Stata_WClog.smcl Stata_WClog.pdf, rmargin(.5)
+translate Stata_WClog.smcl Stata_WClog.txt , replace
+
+
+
+
+
